7a7396d89fd5a05609336b6655f24dea64203a9a
RT Hatfield <arthur_hatfield@homedepot.com>
2017-12-04 12:56:22 -0500

    Day 4 part 1 complete

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,32 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::collections::HashSet;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let f = File::open(&args[1]).unwrap();
+    let file = BufReader::new(&f);
+
+    let mut phrases = 0;
+
+    for line in file.lines() {
+        let l = line.unwrap();
+        let mut memory = HashSet::new();
+        let phrase = l.split_whitespace();
+        let mut count = 0;
+
+        for token in phrase {
+            memory.insert(token.to_string());
+            count = count + 1;
+            println!("{:?}", token);
+        }
+
+        if count == memory.len() {
+            phrases = phrases + 1;
+        }
+    }
+
+    print!("Total count of valid passphrases is {}", phrases);
 }
